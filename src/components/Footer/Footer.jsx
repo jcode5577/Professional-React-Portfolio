c09d17e5dda83d57323243391d67a7c5a01711bc
jcode5577 <99107295+jcode5577@users.noreply.github.com>
2022-09-09 13:20:15 -0500

    make it better

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import "./footer.scss";
-import GitHubIcon from '@material-ui/icons/GitHub';
-import LinkedInIcon from '@material-ui/icons/LinkedIn';
-import TwitterIcon from '@material-ui/icons/Twitter';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import TwitterIcon from '@mui/icons-material/Twitter';
 
 
 function Footer() {
