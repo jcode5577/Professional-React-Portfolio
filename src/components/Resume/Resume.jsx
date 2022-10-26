3a7a898054f054360c1ab76347936bcff3205a0f
jcode5577 <99107295+jcode5577@users.noreply.github.com>
2022-10-26 13:18:49 -0500

    changed link to resume google docs

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -7,7 +7,7 @@ function Resume() {
     return (
         <div className="resume" id="resume">
             <h2>John Hamilton Resume</h2>
-            <p>Note: Please find a <a href="https://drive.google.com/drive/folders/1tSjv0tc_gumLqyroG1DgGZlXIDt1tWrh" target="_blank" rel="noreferrer">downloadable resume here</a></p>
+            <p>Note: Please find a <a href="https://1drv.ms/b/s!AoW8yoP1472ZgV2AMOW3kXA0TKSV?e=1CrGWp" target="_blank" rel="noreferrer">downloadable resume here</a></p>
             <p></p>
             <h3>Full Stack Development Experience</h3>
             <p>As a full stack developer in the KU Extension Full Stack Development Coding Bootcamp, I have developed proficiency with these technologies:</p>
