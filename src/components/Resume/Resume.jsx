8be93f694a2033ce89ed5afba6128e3383a88732
jcode5577 <99107295+jcode5577@users.noreply.github.com>
2022-10-26 14:20:59 -0500

    changed route on resume

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -7,7 +7,7 @@ function Resume() {
     return (
         <div className="resume" id="resume">
             <h2>John Hamilton Resume</h2>
-            <p>Note: Please find a <a href="https://1drv.ms/b/s!AoW8yoP1472ZgV2AMOW3kXA0TKSV?e=1CrGWp" target="_blank" rel="noreferrer">downloadable resume here</a></p>
+            <p>Note: Please find a <a href="https://onedrive.live.com/?cid=99BDE3F583CABC85&id=99bde3f583cabc85%21221&parId=root&o=OneUp" target="_blank" rel="noreferrer">downloadable resume here</a></p>
             <p></p>
             <h3>Full Stack Development Experience</h3>
             <p>As a full stack developer in the KU Extension Full Stack Development Coding Bootcamp, I have developed proficiency with these technologies:</p>
